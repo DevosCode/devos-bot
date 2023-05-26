ae3afbc684a59212b1e75e1b291e3d28fe27f664
Au=Or[79] <assanisaidelamani@gmail.com>
2023-05-26 23:21:11 +0200

    [ADD] - winston, cli-color et implementation d'un service de logger

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const { Client } = require('discord.js');
-const dotenv = require("dotenv");
-dotenv.config();
-const { sequelize } = require('./src/utils/database/models/index.js');
+// // chargement de la base de donnée
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const { sequelize } = require('./src/utils/database/models/index.js');
 
-console.log("[DEVOS-BOT] Lancement de la base de donnée... ... ...");
-(async() => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection réalisé avec succes.');
-    } catch (error) {
-        console.error('Impossible de se connecter à la base de donnée.', error);
-    }
-    try {
-        await sequelize.sync();
-        console.log("La base de donnée à été synchronisé.")
-    }catch (error) {
-        console.error('Impossible de synchronisé la base de donnée.', error);
-    }
-})();
+// console.log("[DEVOS-BOT] Lancement de la base de donnée... ... ...");
+// (async() => {
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection réalisé avec succes.');
+//     } catch (error) {
+//         console.error('Impossible de se connecter à la base de donnée.', error);
+//     }
+//     try {
+//         await sequelize.sync();
+//         console.log("La base de donnée à été synchronisé.")
+//     }catch (error) {
+//         console.error('Impossible de synchronisé la base de donnée.', error);
+//     }
+// })();
 
-// console.log("[DEVOS-BOT] Lancement de Devos-Bot... ... ...")
-// const { Client } = require('discord.js');
+// // console.log("[DEVOS-BOT] Lancement de Devos-Bot... ... ...")
+// // const { Client } = require('discord.js');
 
-// const client = new Client();
+// // const client = new Client();
+
+// // client.login(process.env.TOKEN);
+
+// const Client = require('./src/structure/Client');
+// const config = require('./src/config.json');
+// require('./src/structure/Prototypes');
+// const client = new Client({ config });
 
 // client.login(process.env.TOKEN);
+
+// client.loadEvents();
+// client.loadCommands();
+// client.loadSelectMenus();
