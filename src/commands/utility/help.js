7bb7062dcb518049b662ca11c465f5b7415b83fe
Au=Or[79] <assanisaidelamani@gmail.com>
2023-05-27 15:06:51 +0200

    [ADD] - rep command

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -25,25 +25,12 @@ module.exports = {
         .setTitle('Liste des commandes')
         .setDescription('Toutes les commandes ci-dessous s\'effectuent en slash commande (`/`).')
         .addFields(
-          // { name: `${client.config.emojis.administration} Administration`, value: Object.values(client.commands).filter(c => c.category == 'administration').map(cmd => `\`${cmd.name}\``).join(', ') },
+          { name: `${client.config.emojis.administration} Administration`, value: Object.values(client.commands).filter(c => c.category == 'administration').map(cmd => `\`${cmd.name}\``).join(', ') },
           { name: `${client.config.emojis.economy} Economie`, value: Object.values(client.commands).filter(c => c.category == 'economy').map(cmd => `\`${cmd.name}\``).join(', ') },
           { name: `${client.config.emojis.utility} Utilitaire`, value: Object.values(client.commands).filter(c => c.category == 'utility').map(cmd => `\`${cmd.name}\``).join(', ') }
         )
         .setFooter({ iconURL: client.user.displayAvatarURL(), text: client.config.footer });
 
-      // interaction.reply({
-      //   embeds: [{
-      //     color: client.config.colors.main,
-      //     title: 'Liste des commandes',
-      //     description: 'Toutes les commandes ci-dessous s\'effectuent en slash commande (`/`).',
-      //     fields: [
-      //       { name: `${client.config.emojis.administration} Administration`, value: Object.values(client.commands).filter(c => c.category == 'administration').map(cmd => `\`${cmd.name}\``).join(', ') },
-      //       { name: `${client.config.emojis.economy} Economie`, value: Object.values(client.commands).filter(c => c.category == 'economy').map(cmd => `\`${cmd.name}\``).join(', ') },
-      //       { name: `${client.config.emojis.utility} Utilitaire`, value: Object.values(client.commands).filter(c => c.category == 'utility').map(cmd => `\`${cmd.name}\``).join(', ') }
-      //     ],
-      //     footer: { icon_url: client.user.displayAvatarURL(), text: client.config.footer }
-      //   }]
-      // });
     } else {
       const command = client.commands[commandName.toLowerCase()];
 
