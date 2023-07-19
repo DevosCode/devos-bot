170e354a7527023463a465e9046650c070f6b15d
Au = Or [79] <assanisaidelamani@gmail.com>
2023-07-20 00:23:08 +0200

    [ADD] - possibilite de rendre un contest via un lien

--- src/commands/membre/contest.js
+++ src/commands/membre/contest.js
@@ -14,9 +14,15 @@ module.exports = {
         .setDescription('Soumettre votre travail pour le contest.')
         .addAttachmentOption(option =>
           option
-            .setName('rendu')
+            .setName('File')
             .setDescription('Un fichier zip contenant l\'ensemble de votre composition.')
-            .setRequired(true)
+            .setRequired(false)
+        )
+        .addAttachmentOption(option =>
+          option
+            .setName('Link')
+            .setDescription('Un lien vers un dépôt en ligne permettant de télécharger votre rendu.')
+            .setRequired(false)
         ),
     )
     .addSubcommand(subcommand =>
@@ -41,8 +47,12 @@ module.exports = {
       const name = interaction.options.getString('name');
       await editGuildSetting("CONTEST_NAME", name, interaction.member.guild.id);
       return success(interaction, `J'annonce le début du contest "${name}"!`);
-    }else if (contest_type == "submit") {
-      const rendu = interaction.options.getAttachment('rendu');
+    } else if (contest_type == "submit") {
+      const zipFile = interaction.options.getAttachment('File');
+      const githubLink = interaction.options.getString('Link');
+      if (!zipFile && !githubLink) {
+        return error(interaction, 'Vous devez renseigner une des 2 méthodes de rendu.');
+      }
       const contestChannel = await getSetting("CONTEST_CHANNEL", interaction.guild.id)
       const contestName = await getSetting("CONTEST_NAME", interaction.guild.id)
       if (!contestChannel) {
@@ -52,35 +62,49 @@ module.exports = {
         return error(interaction, 'Le parametre CONTEST_NAME n\'a pas été configuré.');
       }
       const contest_channel = interaction.guild.channels.cache.get(contestChannel.dataValues.value) || await interaction.guild.channels.fetch(contestChannel.dataValues.value).catch(() => null);
-      let thread = await contest_channel.threads.cache.find(x => x.name ==interaction.member.user.username + ` - ` + contestName.dataValues.value); 
+      let thread = await contest_channel.threads.cache.find(x => x.name == interaction.member.user.username + ` - ` + contestName.dataValues.value);
 
       embed = new EmbedBuilder()
         .setColor(client.config.colors.main)
         .setAuthor({ name: interaction.guild.name, iconURL: interaction.guild.iconURL() })
         .setTitle(`${contestName.dataValues.value}`)
         .setFooter({ iconURL: client.user.displayAvatarURL(), text: client.config.footer });
-      
+
       if (!thread) {
         thread = await contest_channel.threads.create({
-          name: interaction.member.user.username + ` - ` + contestName.dataValues.value  ,
+          name: interaction.member.user.username + ` - ` + contestName.dataValues.value,
           autoArchiveDuration: 4320, // 3 jours
           reason: `Soumission d\'un contest par ${interaction.member.user.username}`,
           message: `Voici la proposition de ${interaction.member.toString()} pour le contest: ${contestName.dataValues.value}.`,
-        }); 
-        await thread.send({
-          files: [{
-            attachment : rendu.attachment,
-            name : `Contest de ${interaction.member.user.username} - ${rendu.name}`
-          }]
-        })
-      }else{
-        await thread.send({
-          content : "Une nouvelle soumission de @everyone.",
-          files: [{
-            attachment : rendu.attachment,
-            name : `Contest de ${interaction.member.user.username} - ${rendu.name}`
-          }]
-        })
+        });
+        if (zipFile) {
+          await thread.send({
+            files: [{
+              attachment: zipFile.attachment,
+              name: `Contest de ${interaction.member.user.username} - ${zipFile.name}`
+            }]
+          })
+        }
+        if (githubLink) {
+          thread.send({
+            content: githubLink
+          });
+        }
+      } else {
+        if (zipFile) {
+          await thread.send({
+            content: "Une nouvelle soumission de @everyone.",
+            files: [{
+              attachment: zipFile.attachment,
+              name: `Contest de ${interaction.member.user.username} - ${zipFile.name}`
+            }]
+          })
+        }
+        if (githubLink) {
+          thread.send({
+            content: githubLink
+          });
+        }
       }
       return success(interaction, "Votre proposition a bien été pris en compte!");
     } else {
